6c2dbb09396dc9dfaadf8505edc260eec74a97a9
Calberts4 <42820351+Calberts4@users.noreply.github.com>
2018-11-14 19:08:43 -0800

    Add files via upload

--- practice.js
+++ practice.js
@@ -9,13 +9,18 @@ var out ="-500px";
     document.getElementById("curtain2").style.right = out;
     curtain1.classList.add("invisible");
     curtain2.classList.add("invisible");
+    document.getElementById("smile").style.right = "390px";
+    document.getElementById("smile").style.opacity ="100%"
+    document.getElementById("form").style.left = "390px";
+    document.getElementById("form").style.opacity = "100%";
   }else{
     document.getElementById("curtain1").style.left = "0";
     document.getElementById("curtain2").style.right ="0";
 curtain1.classList.remove("invisible");
 curtain2.classList.remove("invisible");
-
-  }
-
-
-  }
+document.getElementById("smile").style.right = "-600px";
+document.getElementById("smile").style.opacity = "0";
+document.getElementById("form").style.left = "-500px";
+document.getElementById("form").style.opacity ="0"
+}
+}
