01820489c6a63491887237fc46d083d921032857
Calberts4 <42820351+Calberts4@users.noreply.github.com>
2018-11-15 12:43:55 -0800

    Add files via upload

--- practice.js
+++ practice.js
@@ -2,25 +2,35 @@ window.onscroll = function(){
 
   var distance = window.pageYOffset;
   console.log(- distance);
+var bandlogo = document.getElementById("bandlogo");
 var curtain1 = document.getElementById("curtain1");
+var curtain2 = document.getElementById("curtain2");
+var fin = document.getElementById("fin");
+var smile = document.getElementById("smile");
+var form = document.getElementById("form");
+
 var out ="-500px";
-  if(distance >-1){
+  if(distance >5){
+    bandlogo.style.top = "-100px";
+    bandlogo.classList.add("invisible");
     curtain1.style.left = out;
-    document.getElementById("curtain2").style.right = out;
+    curtain2.style.right = out;
     curtain1.classList.add("invisible");
     curtain2.classList.add("invisible");
-    document.getElementById("smile").style.right = "390px";
-    document.getElementById("smile").style.opacity ="100%"
-    document.getElementById("form").style.left = "390px";
-    document.getElementById("form").style.opacity = "100%";
+    smile.style.right = "390px";
+    smile.classList.remove("invisible");
+    form.style.left = "390px";
+    form.classList.remove("invisible");
   }else{
-    document.getElementById("curtain1").style.left = "0";
-    document.getElementById("curtain2").style.right ="0";
-curtain1.classList.remove("invisible");
-curtain2.classList.remove("invisible");
-document.getElementById("smile").style.right = "-600px";
-document.getElementById("smile").style.opacity = "0";
-document.getElementById("form").style.left = "-500px";
-document.getElementById("form").style.opacity ="0"
+    bandlogo.style.top = "100px";
+    bandlogo.classList.remove("invisible");
+    curtain1.style.left = "0";
+    curtain2.style.right ="0";
+    curtain1.classList.remove("invisible");
+    curtain2.classList.remove("invisible");
+    smile.style.right = "-600px";
+    smile.classList.add("invisible");
+    form.style.left = "-500px";
+    form.classList.add("invisible");
 }
 }
